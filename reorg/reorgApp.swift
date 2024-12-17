4fb65e10e5314facebad8f8a1b75b88688da0b81
bill donner <billdonner@gmail.com>
2024-12-17 13:15:48 -0600

    Initial Commit

---
+++ reorg/reorgApp.swift
@@ -0,0 +1,17 @@
+//
+//  reorgApp.swift
+//  reorg
+//
+//  Created by bill donner on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct reorgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
